e21cd884e87e3d9711797f60e6555676c4270f45
Abdalla <90405765+GitAbdalla@users.noreply.github.com>
2024-12-04 05:59:25 +0200

    protect Route

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/authRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
 import dbConnection from "./db/dbConnection.js";
 
 dotenv.config();
@@ -12,8 +14,10 @@ const PORT = process.env.PORT;
 
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.get("/", (req, res) => {
   res.end("Hello");
