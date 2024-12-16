19c13eb74cfb34bae35f1883298965125368f3d8
Abdalla <90405765+GitAbdalla@users.noreply.github.com>
2024-12-17 01:31:16 +0200

    signUp functionality and authentication

---
+++ Frontend/src/context/AuthContext.jsx
@@ -0,0 +1,27 @@
+import { createContext, useContext, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const useAuthContext = ()=>{
+    return useContext(AuthContext);
+}
+
+export const AuthContextProvider = ({ children }) => {
+  const [authUser, setAuthUser] = useState(() => {
+    const user = localStorage.getItem("chat-user");
+    return user ? JSON.parse(user) : null;
+  });
+
+  useEffect(() => {
+    if (!authUser) {
+      localStorage.removeItem("chat-user");
+    }
+  }, [authUser]);
+
+  return (
+    <AuthContext.Provider value={{ authUser, setAuthUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
