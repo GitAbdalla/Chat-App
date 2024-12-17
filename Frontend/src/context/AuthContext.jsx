74cbbb79c766960e3a1b1bac5f1e97a2750e32ce
Abdalla <90405765+GitAbdalla@users.noreply.github.com>
2024-12-17 05:17:23 +0200

    signin functionality and fetching

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import {  useEffect,createContext, useContext, useState } from "react";
 
 export const AuthContext = createContext();
 
