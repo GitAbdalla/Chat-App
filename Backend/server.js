ea395e99189e4e2e29bed0f331c9ab64488a0a2c
Abdalla <90405765+GitAbdalla@users.noreply.github.com>
2024-12-22 23:48:27 +0200

    ready to deploy

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import path from 'path';
 import authRoutes from "./routes/authRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -13,6 +13,7 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 
+const __dirname = path.resolve();
 
 app.use(express.json())
 app.use(cookieParser())
@@ -21,9 +22,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => {
-  res.end("Hello");
-});
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*", (req,res)=>{
+  res.sendFile(path.join(__dirname ,"frontend","dist","index.html"))
+})
+
 
 server.listen(PORT, () => {
   dbConnection();
